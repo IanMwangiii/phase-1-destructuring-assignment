cb62299aad4689ddb05b39d2e7873d00319e517a
Ian <ian.mwangi1@student.moringaschool.com>
2024-04-04 12:22:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -74,4 +74,4 @@ const [, , , , , indg, ,] = colors;
 const { muppetName, color, song, job, partner } = muppet;
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const { nestedJob, nestedPartner} = nestedMuppet;
-const { song1, song2, song4} = nestedMuppet.album.theMuppetMovie;
+const {song2, song4} = nestedMuppet.album.theMuppetMovie;
